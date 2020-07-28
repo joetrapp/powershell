493ca80bc3a45204120705b2135821eddacd2fc8
joetrapp <53957627+joetrapp@users.noreply.github.com>
2020-07-28 15:26:21 -0700

    Commenting

--- PowerCLI/AuditPoweredOffVMs.ps1
+++ PowerCLI/AuditPoweredOffVMs.ps1
@@ -1,3 +1,6 @@
+#### Audit powered off VM's in vCenter
+#### Created: YVFWC-era
+
 Get-VM | Where-Object -Property PowerState -eq "PoweredOff" | Select-Object -Property Name,@{Label="PoweredOffTime"; Expression={
     $_ | Get-ViEvent -Types  Info | Where-Object -Property FullFormattedMessage -Match "shutdown|powered off" |
     Sort-Object -Property CreatedTime  | Select-Object -Last 1 -ExpandProperty CreatedTime
