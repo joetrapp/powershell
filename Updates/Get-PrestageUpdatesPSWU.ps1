382f6c95136bb08736863475d4b4b0cf4b144d53
joetrapp <53957627+joetrapp@users.noreply.github.com>
2020-08-18 20:53:13 -0700

    Spin up Updates for the PSWU module

---
+++ Updates/Get-PrestageUpdatesPSWU.ps1
@@ -0,0 +1,5 @@
+#### Prestage all updates on computer - Requires PSWindowsUpdate
+#### Author: Joe Trapp
+#### Created 8/18/2020
+
+Get-WindowsUpdate –Download
---
+++ Updates/Install-AllUpdatesPSWU.ps1
@@ -0,0 +1,5 @@
+#### Install all updates on computer - Requires PSWindowsUpdate
+#### Author: Joe Trapp
+#### Created 8/18/2020
+
+Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot -Confirm $false
