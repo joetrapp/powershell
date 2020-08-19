e5687599bd5cce5e299f7469d74c9eca7a01faf0
joetrapp <53957627+joetrapp@users.noreply.github.com>
2020-08-18 21:25:03 -0700

    Solution to package not downloading

--- Updates/Install-PSWindowsUpdate.ps1
+++ Updates/Install-PSWindowsUpdate.ps1
@@ -1,1 +1,3 @@
 Install-Module -Name PSWindowsUpdate -Force -SkipPublisherCheck
+# If install fails run this following line:
+# [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
